3bc2f58b923b44ec2c75a35b1028bf1db41a83f3
caionps <caio.nepos@gmail.com>
2021-11-08 16:22:59 -0300

    Atividade Online (Algoritmos) finished

--- Raciocínio Lógico e Algorítmico/atividadeOnline/canvas.js
+++ Raciocínio Lógico e Algorítmico/atividadeOnline/canvas.js
@@ -3,13 +3,47 @@ var canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth
 canvas.height = window. innerHeight 
 
-var c = canvas.getContext('2d') 
+const c = canvas.getContext('2d') 
 
-c.beginPath()
-c.arc(100, 200, 50, 0, Math.PI*2, false)
-c.strokeStyle = 'blue'
-c.fillStyle = 'blue'
-c.fill()
-c.stroke()
+let x = 300
+let y = 300
+let raio = 50
+
+function bola (x, y, raio) {
+    c.beginPath()
+    c.arc(x, y, raio, 0, Math.PI*2, false)
+    c.strokeStyle = 'blue'
+    c.fillStyle = 'blue'
+    c.fill()
+    c.stroke()
+}
+
+canvas.addEventListener('keydown', function(e){
+    switch(e.keyCode) {
+        case 37:
+            console.log('ESQUERDA')
+            x = x - 10
+        break
+        case 38:
+            console.log('CIMA')
+            y = y - 10
+        break
+        case 39:
+            console.log('DIREITA')
+            x = x + 10
+        break
+        case 40:
+            console.log('BAIXO')
+            y = y + 10
+        break
+        default:
+            console.log('ERROOOU')
+    }
+    
+})
+
+setInterval(function(){
+    c.clearRect(0, 0, innerWidth, innerHeight)
+    bola(x,y,raio)}, 1000/60)
 
 
